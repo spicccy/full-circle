206da7081b528b2e7c0be8fe96f4038acda36381
Tony Dong <skithment@gmail.com>
2020-03-10 18:08:49 +1100

    add ui library

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,12 +1,20 @@
 import React, { FunctionComponent } from 'react';
 import { Canvas } from './components/Canvas/Canvas';
+import { Grommet, Box, Heading, Button } from 'grommet';
+import { theme } from './styles/theme';
 
 export const App: FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Canvas />
-      </header>
-    </div>
+    <Grommet theme={theme} full>
+      <Box fill>
+        <Box flex align="center" justify="center">
+          <Box width="medium">
+            <Canvas />
+            <Heading>spicccy.</Heading>
+            <Button label="play" />
+          </Box>
+        </Box>
+      </Box>
+    </Grommet>
   );
 };
