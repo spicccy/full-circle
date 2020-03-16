553c489e12e8c10be5e1414c2e5cdd67a901026b
Alex Su <alexanderytsu@gmail.com>
2020-03-16 12:09:05 +1100

    colocate contextual components

--- packages/frontend/src/App.test.tsx
+++ packages/frontend/src/App.test.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { BrowserRouter as Router } from 'react-router-dom';
 import { App } from './App';
 
 test('renders login page by default', () => {
-  const { getByText } = render(
-    <Router>
-      <App />
-    </Router>
-  );
+  const { getByText } = render(<App />);
   const titleElement = getByText('Login');
   expect(titleElement).toBeInTheDocument();
 });
--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { Grommet } from 'grommet';
 import { theme } from './styles/theme';
 import { RoomProvider } from './contexts/RoomContext';
@@ -10,13 +10,15 @@ import 'styled-components/macro';
 
 export const App: FunctionComponent = () => {
   return (
-    <RoomProvider>
-      <Grommet theme={theme} full>
-        <Switch>
-          <Route exact path="/" component={LoginPage} />
-          <Route path="/play" component={MainPage} />
-        </Switch>
-      </Grommet>
-    </RoomProvider>
+    <Router>
+      <RoomProvider>
+        <Grommet theme={theme} full>
+          <Switch>
+            <Route exact path="/" component={LoginPage} />
+            <Route path="/play" component={MainPage} />
+          </Switch>
+        </Grommet>
+      </RoomProvider>
+    </Router>
   );
 };
