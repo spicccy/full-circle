224f1dadd605f7d8a0f09b54c40a8adb3301e0e3
Tony Dong <skithment@gmail.com>
2020-03-24 14:59:48 +1100

    [NOTICKET] improved debugger (#29)
    * improved debugger
    
    * made text smaller

--- packages/frontend/src/pages/debug/timer/DebugRoomState.tsx
+++ packages/frontend/src/pages/debug/timer/DebugRoomState.tsx
@@ -1,29 +1,58 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 import { useRoomState } from 'src/hooks/useRoomState';
 import styled from 'styled-components';
+import Draggable from 'react-draggable';
+import { useRoom } from 'src/contexts/RoomContext';
+import { useRoomMessage } from 'src/hooks/useRoomMessage';
 
 const Debug = styled.pre`
   position: fixed;
-  top: 0;
-  left: 0;
   background-color: rgba(0, 0, 0, 0.5);
   color: white;
-  padding: 8px;
+  padding: 24px 8px 8px 8px;
   border-radius: 8px;
+  cursor: move;
+  user-select: none;
+  max-height: 300px;
+  overflow-y: auto;
+  font-size: 12px;
 `;
 
 export const DebugRoomState: FunctionComponent<{ debug?: boolean }> = ({
   debug,
 }) => {
+  const [messages, setMessages] = useState<string[]>([]);
+
+  const { room } = useRoom();
   const roomState = useRoomState();
+
+  useEffect(() => {
+    setMessages([]);
+  }, [room]);
+
+  useRoomMessage(message => {
+    setMessages([...messages, message.type]);
+  });
 
   if (!debug) {
     return null;
   }
 
   return (
-    <Debug>
-      {roomState ? JSON.stringify(roomState, undefined, 2) : 'no room'}
-    </Debug>
+    <Draggable>
+      <Debug>
+        Room: {room ? room.id : 'NA'}
+        <br />
+        Session id: {room ? room.sessionId : 'NA'}
+        <br />
+        Room state:
+        <br />
+        {roomState && JSON.stringify(roomState, undefined, 2)}
+        <br />
+        Room messages:
+        <br />
+        {JSON.stringify(messages, undefined, 2)}
+      </Debug>
+    </Draggable>
   );
 };
