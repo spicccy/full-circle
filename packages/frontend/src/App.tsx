9af6639e77f7decf216795742b98aada9c70f99a
WinsonQuan <winson.quan123@hotmail.com>
2020-03-24 14:51:18 +1100

    fix

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { Grommet } from 'grommet';
 import { theme } from './styles/theme';
 import { RoomProvider } from './contexts/RoomContext';
@@ -17,21 +17,26 @@ import 'styled-components/macro';
 import { DebugRoomState } from './pages/debug/timer/DebugRoomState';
 
 export const App: FunctionComponent = () => {
-  return (
-    <RoomProvider>
-      <DebugRoomState debug={true} />
-      <Grommet theme={theme} full>
-        <Switch>
-          <Route exact path="/" component={LoginPage} />
-          <Route exact path="/join" component={LoginPage} />
-          <Route exact path="/home" component={HomePage} />
-          <Route exact path="/play" component={PlayerGamePage} />
-          <Route exact path="/game" component={CuratorGamePage} />
-          <Route exact path="/instructions" component={Instructions} />
-          <Route exact path="/team" component={Team} />
-          <Route exact path="/timertest" component={TimerTest} />
-        </Switch>
-      </Grommet>
-    </RoomProvider>
-  );
+  try {
+    return (
+      <RoomProvider>
+        <DebugRoomState debug={true} />
+        <Grommet theme={theme} full>
+          <Switch>
+            <Route exact path="/" component={LoginPage} />
+            <Route exact path="/join" component={LoginPage} />
+            <Route exact path="/home" component={HomePage} />
+            <Route exact path="/play" component={PlayerGamePage} />
+            <Route exact path="/game" component={CuratorGamePage} />
+            <Route exact path="/instructions" component={Instructions} />
+            <Route exact path="/team" component={Team} />
+            <Route exact path="/timertest" component={TimerTest} />
+          </Switch>
+        </Grommet>
+      </RoomProvider>
+    );
+  } catch (e) {
+    console.error(e);
+    return <Redirect to="/" />;
+  }
 };
