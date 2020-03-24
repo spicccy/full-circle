081ee18a5a141ed8dbd7ca9361cf179bc2ef16aa
WinsonQuan <winson.quan123@hotmail.com>
2020-03-24 14:26:29 +1100

    restructing

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -3,7 +3,14 @@ import { Switch, Route, Redirect } from 'react-router-dom';
 import { Grommet } from 'grommet';
 import { theme } from './styles/theme';
 import { RoomProvider } from './contexts/RoomContext';
-import { LoginPage, GamePage, Instructions, Team, HomePage } from './pages';
+import {
+  LoginPage,
+  GamePage,
+  Instructions,
+  Team,
+  HomePage,
+  TimerTest,
+} from './pages';
 
 import 'styled-components/macro';
 import { DebugRoomState } from './pages/debug/timer/DebugRoomState';
@@ -20,6 +27,7 @@ export const App: FunctionComponent = () => {
           <Route exact path="/play" component={GamePage} />
           <Route exact path="/instructions" component={Instructions} />
           <Route exact path="/team" component={Team} />
+          <Route exact path="/timertest" component={TimerTest} />
         </Switch>
       </Grommet>
     </RoomProvider>
