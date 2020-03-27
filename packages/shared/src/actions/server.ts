eeed280b87351f03518a84364864efa526e7647a
mismo123 <41666374+mismo123@users.noreply.github.com>
2020-03-27 15:26:09 +1100

    connected backend to broadcast guesses as prompts (#38)
    Co-authored-by: z5116810 <z5116810@ad.unsw.edu.au>

--- packages/shared/src/actions/server.ts
+++ packages/shared/src/actions/server.ts
@@ -5,3 +5,5 @@ import { CanvasAction } from '../canvas/interfaces';
 export const displayDrawing = createAction('@server/displayDrawing')<
   CanvasAction[]
 >();
+
+export const displayPrompt = createAction('@server/displayPrompt')<string>();
