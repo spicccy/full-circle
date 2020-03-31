740c9f843ed04279e24dd309050eb0ca98276bf5
Tony Dong <skithment@gmail.com>
2020-03-31 11:43:15 +1100

    [SPICE-90] added more canvas functionality (#46)
    * added more canvas functionality
    
    * refactored keydown listener
    
    * fixed css

--- packages/frontend/src/pages/player/game/draw/CanvasCard.tsx
+++ packages/frontend/src/pages/player/game/draw/CanvasCard.tsx
@@ -1,13 +1,47 @@
 import 'styled-components/macro';
 
-import { CanvasAction, Pen } from '@full-circle/shared/lib/canvas';
+import {
+  CanvasAction,
+  clearCanvas,
+  Colour,
+  Pen,
+} from '@full-circle/shared/lib/canvas';
 import { Box, Heading } from 'grommet';
 import React, { FunctionComponent } from 'react';
+import { BaseButton } from 'src/components/BaseButton';
 import { Canvas } from 'src/components/Canvas/Canvas';
 import { Card } from 'src/components/Card/Card';
+import { useEventListener } from 'src/hooks/useEventListener';
+import styled from 'styled-components/macro';
 
 import { BorderBottom } from '../components/BorderBottom';
 import { SubmitButton } from '../components/SubmitButton';
+import { ReactComponent as Delete } from '../icons/delete.svg';
+// import { ReactComponent as Redo } from '../icons/redo.svg';
+import { ReactComponent as Undo } from '../icons/undo.svg';
+
+const ButtonWrapper = styled(BaseButton)`
+  padding: 4px;
+  transition: fill 0.2s;
+  fill: ${Colour.DARK_GRAY};
+
+  :disabled {
+    fill: ${Colour.LIGHT_GRAY};
+  }
+
+  :focus {
+    outline: 2px dashed black;
+    z-index: 1;
+  }
+`;
+
+const EraserWrapper = styled(ButtonWrapper)`
+  margin-left: 4px;
+
+  :hover:enabled {
+    fill: ${Colour.RED};
+  }
+`;
 
 interface ICanvasCardProps {
   pen: Pen;
@@ -16,12 +50,44 @@ interface ICanvasCardProps {
   onSubmitDrawing(): void;
 }
 
+// TODO: (Tony) implement redo
 const CanvasCard: FunctionComponent<ICanvasCardProps> = ({
   pen,
   canvasActions,
   setCanvasActions,
   onSubmitDrawing,
 }) => {
+  const isEmpty = canvasActions.length === 0;
+
+  const undo = () =>
+    setCanvasActions(canvasActions.slice(0, canvasActions.length - 1));
+  const clear = () => setCanvasActions([...canvasActions, clearCanvas()]);
+
+  useEventListener(document, 'keydown', (e) => {
+    if (e.ctrlKey) {
+      switch (e.key) {
+        case 'Enter':
+          e.preventDefault();
+          if (!isEmpty) {
+            onSubmitDrawing();
+          }
+          break;
+        case 'z':
+          e.preventDefault();
+          if (!isEmpty) {
+            undo();
+          }
+          break;
+        case 'd':
+          e.preventDefault();
+          if (!isEmpty) {
+            clear();
+          }
+          break;
+      }
+    }
+  });
+
   return (
     <Card>
       <BorderBottom
@@ -34,7 +100,7 @@ const CanvasCard: FunctionComponent<ICanvasCardProps> = ({
           canvasActions={canvasActions}
           setCanvasActions={setCanvasActions}
         />
-        {canvasActions.length === 0 && (
+        {isEmpty && (
           <Heading
             level="2"
             css={{
@@ -48,9 +114,23 @@ const CanvasCard: FunctionComponent<ICanvasCardProps> = ({
         )}
       </BorderBottom>
 
-      <Box>
+      <Box direction="row" align="center" pad={{ left: '4px', right: '4px' }}>
+        <ButtonWrapper title="undo (ctrl-z)" onClick={undo} disabled={isEmpty}>
+          <Undo />
+        </ButtonWrapper>
+        {/* <ButtonWrapper title="redo (ctrl-y)">
+          <Redo />
+        </ButtonWrapper> */}
+        <EraserWrapper
+          title="clear (ctrl-d)"
+          onClick={clear}
+          disabled={isEmpty}
+        >
+          <Delete />
+        </EraserWrapper>
         <SubmitButton
-          disabled={!canvasActions.length}
+          title="submit (ctrl-enter)"
+          disabled={isEmpty}
           onClick={onSubmitDrawing}
           label="Submit"
         />
