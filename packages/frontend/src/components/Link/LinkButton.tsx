fb687512c1227f99a9c527f7daa4c58d37ee22b4
Tony Dong <skithment@gmail.com>
2020-03-31 17:53:06 +1100

    added misc cleanup

--- packages/frontend/src/components/Link/LinkButton.tsx
+++ packages/frontend/src/components/Link/LinkButton.tsx
@@ -1,17 +1,22 @@
+import { MaybePromise } from '@full-circle/shared/lib/interfaces';
 import { Button, ButtonProps } from 'grommet';
 import React, { FunctionComponent, MouseEventHandler } from 'react';
 import { useHistory } from 'react-router-dom';
 
 interface ILinkButtonProps extends ButtonProps {
   href: string;
+  onClick?(): MaybePromise<boolean | void>;
 }
 
 const LinkButton: FunctionComponent<ILinkButtonProps> = (props) => {
   const history = useHistory();
 
-  const overrideHref: MouseEventHandler = (e) => {
+  const overrideHref: MouseEventHandler = async (e) => {
     e.preventDefault();
-    history.push(props.href);
+    const shouldNavigate = (await props.onClick?.()) !== false;
+    if (shouldNavigate) {
+      history.push(props.href);
+    }
   };
 
   return <Button {...props} onClick={overrideHref} />;
