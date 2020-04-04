6a8856a6a3fc8a1b73b97b1ea48278b7fd4d23f4
Alex Su <alexanderytsu@gmail.com>
2020-04-04 19:15:30 +1100

    frontend build

--- packages/frontend/src/contexts/ColyseusContext.ts
+++ packages/frontend/src/contexts/ColyseusContext.ts
@@ -2,6 +2,7 @@ import { BACKEND_PORT } from '@full-circle/shared/lib/constants';
 import * as Colyseus from 'colyseus.js';
 import { createContext, useContext } from 'react';
 
-const client = new Colyseus.Client(`ws://localhost:${BACKEND_PORT}`);
+// const client = new Colyseus.Client(`ws://localhost:${BACKEND_PORT}`);
+const client = new Colyseus.Client(`wss://fullcircle-backend.herokuapp.com`);
 export const ColyseusContext = createContext(client);
 export const useColyseus = () => useContext(ColyseusContext);
