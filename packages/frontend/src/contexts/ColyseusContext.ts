41ced9f226d8cda3833faf1dd47663ffc92ff078
Tony Dong <skithment@gmail.com>
2020-04-10 23:05:12 +1000

    removed guesser and drawer names

--- packages/frontend/src/contexts/ColyseusContext.ts
+++ packages/frontend/src/contexts/ColyseusContext.ts
@@ -2,8 +2,11 @@ import { BACKEND_PORT } from '@full-circle/shared/lib/constants';
 import * as Colyseus from 'colyseus.js';
 import { createContext, useContext } from 'react';
 
-const client = process.env.REACT_APP_BACKEND_URL
-  ? new Colyseus.Client(`${process.env.REACT_APP_BACKEND_URL}`)
-  : new Colyseus.Client(`ws://localhost:${BACKEND_PORT}`);
+const colyseusUrl =
+  process.env.REACT_APP_BACKEND_LOCALHOST === 'true'
+    ? `ws://localhost:${BACKEND_PORT}`
+    : process.env.REACT_APP_BACKEND_URL;
+
+const client = new Colyseus.Client(colyseusUrl);
 export const ColyseusContext = createContext(client);
 export const useColyseus = () => useContext(ColyseusContext);
