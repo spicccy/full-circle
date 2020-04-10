58c42dde89047cabeb275e774380a765f6f319c3
Alex Su <alexanderytsu@gmail.com>
2020-04-10 22:25:29 +1000

    hello

--- packages/frontend/src/contexts/ColyseusContext.ts
+++ packages/frontend/src/contexts/ColyseusContext.ts
@@ -2,8 +2,9 @@ import { BACKEND_PORT } from '@full-circle/shared/lib/constants';
 import * as Colyseus from 'colyseus.js';
 import { createContext, useContext } from 'react';
 
-const client = process.env.REACT_APP_BACKEND_URL
-  ? new Colyseus.Client(`${process.env.REACT_APP_BACKEND_URL}`)
-  : new Colyseus.Client(`ws://localhost:${BACKEND_PORT}`);
+const client =
+  process.env.REACT_APP_DEPLOYED === 'true'
+    ? new Colyseus.Client(`${process.env.REACT_APP_BACKEND_URL}`)
+    : new Colyseus.Client(`ws://localhost:${BACKEND_PORT}`);
 export const ColyseusContext = createContext(client);
 export const useColyseus = () => useContext(ColyseusContext);
