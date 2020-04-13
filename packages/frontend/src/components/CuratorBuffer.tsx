56b4b7a52bac98059145325af0f0aeb67a1060f9
WinsonQuan <winson.quan123@hotmail.com>
2020-04-13 17:56:37 +1000

    buffer page

---
+++ packages/frontend/src/components/CuratorBuffer.tsx
@@ -0,0 +1,10 @@
+import { Box, Text } from 'grommet';
+import React, { FunctionComponent } from 'react';
+
+export const CuratorBuffer: FunctionComponent = () => {
+  return (
+    <Box fill align="center" justify="center">
+      <Text>We will begin the next state shortly.</Text>
+    </Box>
+  );
+};
