d9911f9fd53a6120c275366bd613d99b534589e1
Tony Dong <skithment@gmail.com>
2020-04-17 22:20:34 +1000

    Update util.ts

--- packages/backend/src/util/util.ts
+++ packages/backend/src/util/util.ts
@@ -2,4 +2,4 @@ export const shuffle = <T>(arr: T[]): T[] =>
   [...arr].sort(() => Math.random() - 0.5);
 
 export const closeEnough = (a?: string, b?: string) =>
-  a && b && a.toLowerCase().trim() === b.toLowerCase().trim();
+  Boolean(a && b && a.toLowerCase().trim() === b.toLowerCase().trim());
