e64c0a00ee09888263f9e3113d0d54cce77dab8e
WinsonQuan <winson.quan123@hotmail.com>
2020-04-20 14:00:19 +1000

    refactor curator timer

--- packages/frontend/src/components/CuratorTimer.tsx
+++ packages/frontend/src/components/CuratorTimer.tsx
@@ -1,19 +1,14 @@
 import { Text } from 'grommet';
 import React, { FunctionComponent } from 'react';
 import { usePhaseTimer } from 'src/hooks/usePhaseTimer';
-import { useRoomHelpers } from 'src/hooks/useRoomHelpers';
 
 export const CuratorTimer: FunctionComponent = () => {
   const phaseTimer = usePhaseTimer();
-  const { allSubmitted } = useRoomHelpers();
 
   if (phaseTimer === undefined) {
     return null;
   }
   if (phaseTimer < -1) {
-    return null;
-  }
-  if (allSubmitted) {
     return null;
   }
   return <Text data-testid="curatorTimer"> Timer: {phaseTimer}</Text>;
--- packages/frontend/src/pages/curator/IngamePages/IngameDraw.tsx
+++ packages/frontend/src/pages/curator/IngamePages/IngameDraw.tsx
@@ -9,18 +9,28 @@ import { useRoom } from 'src/contexts/RoomContext';
 const IngameDraw: FunctionComponent = () => {
   const { syncedState } = useRoom();
 
-  let title = <Heading>Guessing Phase</Heading>;
-  let text = <Paragraph>It's time to g-g-g-g-g-g-g-guess</Paragraph>;
-  if (syncedState?.showBuffer) {
-    title = <Heading> Transitioning </Heading>;
-    text = <Paragraph>We will begin the next stage shortly.</Paragraph>;
-  }
+  const renderBody = () => {
+    if (syncedState?.showBuffer) {
+      return (
+        <Box>
+          <Heading> Transitioning </Heading>
+          <Paragraph>We will begin the next stage shortly.</Paragraph>
+        </Box>
+      );
+    }
+    return (
+      <Box>
+        <Heading>Guessing Phase</Heading>
+        <Paragraph>It's time to g-g-g-g-g-g-g-guess</Paragraph>
+        <CuratorTimer />
+      </Box>
+    );
+  };
+
   return (
     <Box css={{ position: 'relative' }} flex>
       <Box flex align="center" justify="center">
-        {title}
-        {text}
-        <CuratorTimer />
+        {renderBody()}
         <LinkButton alignSelf="center" label="Go to Home" href="/create" />
       </Box>
     </Box>
