9e1b09a5b9446ea0aff59492a3e41cd57acc27c2
z5116810 <z5116810@ad.unsw.edu.au>
2020-04-20 21:54:28 +1000

    populated meet the team page with templates

--- packages/frontend/src/pages/shared/Team.tsx
+++ packages/frontend/src/pages/shared/Team.tsx
@@ -1,19 +1,89 @@
-import { Box, Heading, Paragraph } from 'grommet';
+import { Box, Heading, Paragraph, Grommet } from 'grommet';
 import React, { FunctionComponent } from 'react';
 import { Navbar } from 'src/components/Navbar';
+import { theme } from 'src/styles/theme';
+import { StickyNote } from 'src/components/StickyNote';
+import { StickyNoteColour } from '@full-circle/shared/lib/roomState';
 
 const Team: FunctionComponent = () => {
   return (
-    <Box background="light-2" flex>
+    <Box fill flex>
       <Navbar />
-      <Box align="center" justify="start">
-        <Box width="medium">
-          <Heading>Team Members</Heading>
+      <Box flex align="center" justify="center">
+        <Box flex width="large" pad="medium" align="center">
+          <Heading level={1}>Team Members</Heading>
           <Box align="start">
-            <Heading level="2" margin="none">
-              Alexander Su
-            </Heading>
-            <Paragraph>Hi I am wholesome.</Paragraph>
+            <StickyNote
+              colour={StickyNoteColour.RED}
+              size="medium"
+              pad="medium"
+              randomData={{ angle: 2, bendAmountLeft: 1, bendAmountRight: 0 }}
+            >
+              <Grommet theme={theme}>
+                <Heading level="2">Alexander Su</Heading>
+                <Paragraph>
+                  Hi I am wholesome. I am like really good at coding and you
+                  will always find me cool, calm and collected. Also, everyone
+                  is hacking except me and I hate yml.
+                </Paragraph>
+              </Grommet>
+            </StickyNote>
+            <StickyNote
+              colour={StickyNoteColour.YELLOW}
+              size="medium"
+              pad="medium"
+              randomData={{ angle: -1, bendAmountLeft: 0, bendAmountRight: 2 }}
+            >
+              <Grommet theme={theme}>
+                <Heading level="2">Tony Dong</Heading>
+                <Paragraph>
+                  Filler. Filler. Filler. Filler. Filler. Filler. Filler.
+                  Filler. Filler. Filler. Filler. Filler. Filler.
+                </Paragraph>
+              </Grommet>
+            </StickyNote>
+            <StickyNote
+              colour={StickyNoteColour.CYAN}
+              size="medium"
+              pad="medium"
+              randomData={{ angle: 4, bendAmountLeft: 1, bendAmountRight: -1 }}
+            >
+              <Grommet theme={theme}>
+                <Heading level="2">Winson Quan</Heading>
+                <Paragraph>
+                  Filler. Filler. Filler. Filler. Filler. Filler. Filler.
+                  Filler. Filler. Filler. Filler. Filler. Filler.
+                </Paragraph>
+              </Grommet>
+            </StickyNote>
+            <StickyNote
+              colour={StickyNoteColour.GREEN}
+              size="medium"
+              pad="medium"
+              randomData={{ angle: 2, bendAmountLeft: 1, bendAmountRight: 0 }}
+            >
+              <Grommet theme={theme}>
+                <Heading level="2">Daniel Huang</Heading>
+                <Paragraph>
+                  Filler. Filler. Filler. Filler. Filler. Filler. Filler.
+                  Filler. Filler. Filler. Filler. Filler. Filler.
+                </Paragraph>
+              </Grommet>
+            </StickyNote>
+            <StickyNote
+              colour={StickyNoteColour.BLUE}
+              size="medium"
+              pad="medium"
+              randomData={{ angle: -3, bendAmountLeft: -2, bendAmountRight: 1 }}
+            >
+              <Grommet theme={theme}>
+                <Heading level="2">Damien Chan</Heading>
+                <Paragraph>
+                  Filler. Filler. Filler. Filler. Filler. Filler. Filler.
+                  Filler. Filler. Filler. Filler. Filler. Filler.
+                </Paragraph>
+              </Grommet>
+            </StickyNote>
           </Box>
         </Box>
       </Box>
