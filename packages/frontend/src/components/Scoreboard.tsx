4df924dfacd3ad5c99ac672fe82068ae3188164d
Tony Dong <skithment@gmail.com>
2020-04-28 03:06:28 +1000

    add end game screen for players

--- packages/frontend/src/components/Scoreboard.tsx
+++ packages/frontend/src/components/Scoreboard.tsx
@@ -1,6 +1,7 @@
 import 'styled-components/macro';
 
 import { Colour } from '@full-circle/shared/lib/canvas';
+import { objectValues } from '@full-circle/shared/lib/helpers';
 import { IPlayer } from '@full-circle/shared/lib/roomState';
 import { Box, Grommet, Heading, Text } from 'grommet';
 import React, { FunctionComponent } from 'react';
@@ -8,23 +9,13 @@ import React, { FunctionComponent } from 'react';
 import { useRoom } from '../contexts/RoomContext';
 import { notepadTheme } from '../styles/notepadTheme';
 
-const Scoreboard: FunctionComponent = () => {
-  const { syncedState } = useRoom();
+interface IScoreboardProps {
+  players: IPlayer[];
+}
 
-  if (!syncedState) {
-    return null;
-  }
-  const players = syncedState.playerManager.playerMap;
-
-  const playerScores: IPlayer[] = [];
-
-  for (const id in syncedState.playerManager.playerMap) {
-    const player: IPlayer = players[id];
-    playerScores.push(player);
-  }
-
+const Scoreboard: FunctionComponent<IScoreboardProps> = ({ players }) => {
   const renderScoreboardRows = () => {
-    return playerScores
+    return players
       .sort((a, b) => b.score - a.score)
       .map((player) => {
         return (
@@ -36,6 +27,34 @@ const Scoreboard: FunctionComponent = () => {
         );
       });
   };
+
+  return (
+    <div
+      css={{
+        display: 'grid',
+        gridTemplateColumns: '2fr 1fr 1fr',
+        paddingBottom: 24,
+      }}
+    >
+      <Text size="large">Player</Text>
+      <Text size="large" textAlign="end">
+        Score
+      </Text>
+      <Text size="large" textAlign="end">
+        Votes
+      </Text>
+      {renderScoreboardRows()}
+    </div>
+  );
+};
+
+const ScoreboardPage: FunctionComponent = () => {
+  const { syncedState } = useRoom();
+
+  if (!syncedState) {
+    return null;
+  }
+  const players = objectValues(syncedState.playerManager.playerMap);
 
   return (
     <Grommet theme={notepadTheme}>
@@ -56,25 +75,10 @@ const Scoreboard: FunctionComponent = () => {
         <Heading level="2" textAlign="center">
           Scoreboard
         </Heading>
-        <div
-          css={{
-            display: 'grid',
-            gridTemplateColumns: '2fr 1fr 1fr',
-            paddingBottom: 24,
-          }}
-        >
-          <Text size="large">Player</Text>
-          <Text size="large" textAlign="end">
-            Score
-          </Text>
-          <Text size="large" textAlign="end">
-            Votes
-          </Text>
-          {renderScoreboardRows()}
-        </div>
+        <Scoreboard players={players} />
       </Box>
     </Grommet>
   );
 };
 
-export { Scoreboard };
+export { Scoreboard, ScoreboardPage };
